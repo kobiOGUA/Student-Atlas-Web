24adf6094f46a5945ea6d25cd2a825b1d18247cb
Kobi Oguadinma <kobioguadinma@gmail.com>
2026-01-15 20:22:48 +0100

    Configure upload function for Vercel deployment (Option 1)

--- OneDrive/Desktop/Test 500/web-app/js/firebase-config.js
+++ OneDrive/Desktop/Test 500/web-app/js/firebase-config.js
@@ -4,6 +4,7 @@
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js';
 import { getAuth } from 'https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js';
 import { getFirestore, enableIndexedDbPersistence } from 'https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js';
+import { getStorage } from 'https://www.gstatic.com/firebasejs/10.12.0/firebase-storage.js';
 
 const firebaseConfig = {
     apiKey: "AIzaSyBKA2julrxpRhXTdU_8l5pSH73ERAjKtO4",
@@ -19,6 +20,7 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+export const storage = getStorage(app);
 
 // Enable offline persistence
 enableIndexedDbPersistence(db).catch((err) => {
