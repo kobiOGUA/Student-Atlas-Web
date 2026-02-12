791dba2b4084e74ab880063f729213f9f7584f1d
Kobi Oguadinma <kobioguadinma@gmail.com>
2026-02-12 09:52:57 +0100

    Remove accidental recursive OneDrive directory

--- OneDrive/Desktop/Test 500/web-app/js/services/messaging.js
+++
@@ -1,8 +0,0 @@
-// Messaging Service - Placeholder
-export async function getConversations(userId) {
-    return [];
-}
-
-export async function sendMessage(fromId, toId, content) {
-    return true;
-}
--- OneDrive/Desktop/Test 500/web-app/js/utils/encryption.js
+++
@@ -1,8 +0,0 @@
-// PIN Encryption using CryptoJS
-export function hashPINWithSalt(pin, salt) {
-    return CryptoJS.SHA256(pin + salt).toString();
-}
-
-export function generateSalt() {
-    return CryptoJS.lib.WordArray.random(128 / 8).toString();
-}
